58244e0f78bf598d9c7e4a35c6be68aafa61b59a
zebats <1348329118@qq.com>
2022-09-08 23:31:04 +0800

    .

--- sm18-lazy-package-1.2.2/CET4/Card.py
+++ sm18-lazy-package-1.2.2/CET4/Card.py
@@ -6,7 +6,9 @@ import os
 import urllib.request
 import jieba
 from subprocess import Popen
+from fake_useragent import UserAgent
 
+headers={"User-Agent":UserAgent().firefox}
 with open('dic.txt','r') as f:
     dic=json.loads(f.read())
 with open('sdic.txt','r',encoding='utf-8') as f:
@@ -21,11 +23,11 @@ def cmp(a):
     return len(a[0])
 
 def getTranslation(word):
-    con=requests.get(f'https://dict.youdao.com/w/eng/{word}/#keyfrom=dict2.index').text
+    con=requests.get(url=f'https://dict.youdao.com/w/eng/{word}/#keyfrom=dict2.index',headers=headers).text
     return [re.search(r'(?<=<li>)[a-z]+\.[^；]+?(?=(<\/li>|\n|；))',con).group(),re.search(r'(?<=<ul>\n     <li>)(.|\s)+?(?=<\/li>\n    <\/ul>)',con).group().replace('</li>','').replace('<li>','')]#[simplified/full]
 
 def getSentence(word):
-    con=requests.get(f'https://dict.youdao.com/w/eng/lj:{word}/#keyfrom=dict2.index').text
+    con=requests.get(url=f'https://dict.youdao.com/w/eng/lj:{word}/#keyfrom=dict2.index',headers=headers).text
     soup=BeautifulSoup(con,"html.parser")
     a=soup.find('ul',class_="ol").contents
     b=[i for i in a if i!='\n'] 
@@ -67,6 +69,13 @@ for key in dic: #选词
             print(f'no sentences found!({key})')
             continue
         meaning=getTranslation(key)
+        meaningskey=[]
+        for j in sdic[key.lower()]:
+            if not re.search(r'[一-龟]',j):
+                wordType=j
+            else :
+                meaningskey.append([wordType+j,0])
+        meaningskey.append(['',1])
         for i in sentence:#词频统计
             j=jieba.cut(i[1])
             for k in j:
@@ -87,17 +96,74 @@ for key in dic: #选词
         # a=[(k,v) for k,v in ci.items()].sort(key=lambda x : x[1])
         while True: #选句子1
             print('-'*20+'\n高频词: '+top+'\n'+sentence[n][0],'|',key,'|','  '.join(sdic[key]),'|',re.sub(r'\n',rf"\n{' '*(9+len(sentence[n][0])+len(key)+len('  '.join(sdic[key])))}",meaning[1]),'\n',sentence[n][1])
-            change=input('change?(1/2/3)')
+            #1:切换下一个句子 2:选择当前的句子 3：回到第一个句子 4:切换到下一个单词
+            change=input('change?(1/2/3/4)')
             if change=='1':
                 if n==len(sentence)-2:
+                    print('-'*20+'\n  over!\n'+'-'*20+'/n')
                     n=0
                 else:
                     n+=1
-            elif change=='2':#改这里的时候也要改下面的
+            # elif change=='2':#改这里的时候也要改下面的（没有下面了现在）
+            #     for i in sentence[n][0].replace('.','').replace('?','').split(): #处理句子
+            #         if i.lower() in tempdic and tempdic[i.lower()]==1 or i in tempdic and tempdic[i]==1 or re.search(r"'",i.lower()):
+            #             continue
+            #         if re.search(r'[0-9]',i) or re.search("'",i):
+            #             continue
+            #         print(i)
+            #         if not i.lower() in tempdic:
+            #             # if re.match(r'^\w+$',i.lower()) and not os.path.exists(f'./pronunciation/{i.lower()}'):#下载发音
+            #             #     os.makedirs(f'./pronunciation/{i.lower()}')
+            #             #     urllib.request.urlretrieve("http://dict.youdao.com/dictvoice?type=1&audio="+i.lower(),f".\pronunciation\{i.lower()}\{i.lower()}.mp3")
+            #             inpu=input('"0N/1Y/2Pass"\n')
+            #             if inpu=='2':
+            #                 continue
+            #             if inpu!='0':
+            #                 tempdic[i.lower()]=1
+            #                 continue
+            #         # tempdic[i.lower()]=1
+            #         else:
+            #             meanings=[]
+            #             for j in sdic[i.lower()]:
+            #                 if not re.search(r'[一-龟]',j):
+            #                     wordType=j
+            #                 else :
+            #                     meanings.append(wordType+j)
+            #             meanings.append('')
+            #         trans=getTranslation(i)
+            #         print(trans[1])
+            #         with open('eng.htm','a',encoding="utf-8") as f: #保存数据
+            #             if i.lower()in tempdic and tempdic[i.lower()]==0:
+            #                 for j in range(len(meanings)):
+            #                     print(j,meanings[j])
+            #                 # a=re.search(r'[a-z]+?\.',trans[0]).group() #就是提取个词性
+            #                 an=input('which?')
+            #                 if an=='':
+            #                     an='0'
+            #                 f.write(f'Q: {sentence[n][0].replace(i,f"<a style={oneCharacter}display:none{oneCharacter}>.</a><b><u>{i}</u></b><a style={oneCharacter}display:none{oneCharacter}>.</a>")}\nA: {meanings[int(an)]} \n<hr>\n\n')
+            #                 # if i.lower()!=key:
+            #                 #     tempdic[i.lower()]=1
+            #             else:
+            #                 f.write(f'Q: {sentence[n][0].replace(i,f"<a style{oneCharacter}display:none{oneCharacter}>.</a><b><u>{i}</u></b><a style={oneCharacter}display:none{oneCharacter}>.</a>")}\nA: {trans[0]} \n<hr>\n\n')
+            #         with open('dic.txt','r') as f:
+            #             temptempdic=json.loads(f.read())
+            #         for j in tempdic:
+            #             if not j in temptempdic:
+            #                 temptempdic[j]=tempdic[j]
+            #         temptempdic[i.lower()]=1
+            #         with open('dic.txt','w') as f:
+            #             f.write(json.dumps(temptempdic))
+            #     n+=1
+            elif change == '3':
+                n = 0
+            elif change == '4':
+                tempdic[key]=1
+                break
+            else:#原来的2
                 for i in sentence[n][0].replace('.','').replace('?','').split(): #处理句子
                     if i.lower() in tempdic and tempdic[i.lower()]==1 or i in tempdic and tempdic[i]==1 or re.search(r"'",i.lower()):
                         continue
-                    if re.search(r'[0-9]',i) or re.search("'",i):
+                    if not re.search(r'^[a-zA-Z]+$',i):
                         continue
                     print(i)
                     if not i.lower() in tempdic:
@@ -111,89 +177,102 @@ for key in dic: #选词
                             tempdic[i.lower()]=1
                             continue
                     # tempdic[i.lower()]=1
-                    else:
-                        meanings=[]
+                    if i.lower() != key and i.lower() in sdic:
+                        meaningsnow=[]
                         for j in sdic[i.lower()]:
                             if not re.search(r'[一-龟]',j):
                                 wordType=j
                             else :
-                                meanings.append(wordType+j)
-                        meanings.append('')
+                                meaningsnow.append([wordType+j,0])
+                        meaningsnow.append(['',1])
+                        meanings=meaningsnow
+                    elif i.lower() == key:
+                        meanings=meaningskey
                     trans=getTranslation(i)
                     print(trans[1])
                     with open('eng.htm','a',encoding="utf-8") as f: #保存数据
-                        if tempdic[i.lower()]==0:
+                        if i.lower() in sdic:
                             for j in range(len(meanings)):
-                                print(j,meanings[j])
+                                print(j,meanings[j][0])
                             # a=re.search(r'[a-z]+?\.',trans[0]).group() #就是提取个词性
                             an=input('which?')
                             if an=='':
                                 an='0'
-                            f.write(f'Q: {sentence[n][0].replace(i,f"<a style={oneCharacter}display:none{oneCharacter}>.</a><b><u>{i}</u></b><a style={oneCharacter}display:none{oneCharacter}>.</a>")}\nA: {meanings[int(an)]} \n<hr>\n\n')
+                            f.write(f'Q: {sentence[n][0].replace(i,f"<a style={oneCharacter}display:none{oneCharacter}>.</a><b><u>{i}</u></b><a style={oneCharacter}display:none{oneCharacter}>.</a>")}\nA: {meanings[int(an)][0]}\n<hr>\n\n')
                             # if i.lower()!=key:
                             #     tempdic[i.lower()]=1
+                            meanings.pop(int(an))
                         else:
-                            f.write(f'Q: {sentence[n][0].replace(i,f"<a style{oneCharacter}display:none{oneCharacter}>.</a><b><u>{i}</u></b><a style={oneCharacter}display:none{oneCharacter}>.</a>")}\nA: {trans[0]} \n<hr>\n\n')
+                            f.write(f'Q: {sentence[n][0].replace(i,f"<a style={oneCharacter}display:none{oneCharacter}>.</a><b><u>{i}</u></b><a style={oneCharacter}display:none{oneCharacter}>.</a>")}\nA: {trans[0]}\n<hr>\n\n')
                     with open('dic.txt','r') as f:
                         temptempdic=json.loads(f.read())
                     for j in tempdic:
                         if not j in temptempdic:
                             temptempdic[j]=tempdic[j]
-                    temptempdic[i.lower()]=1
+                    if i.lower() == key :
+                        temptempdic[i.lower()]=1
                     with open('dic.txt','w') as f:
                         f.write(json.dumps(temptempdic))
+                completed=1
+                for i in meaningskey:
+                    completed &= i[1]
+                if completed :
+                    break
                 n+=1
-            elif change == '3':
-                n = 0
-            else:
-                break
-        print(f'{totalc-count} left',count)
-        for i in sentence[n][0].replace('.','').replace('?','').split(): #处理句子
-            if i.lower() in tempdic and tempdic[i.lower()]==1 or i in tempdic and tempdic[i]==1 or re.search(r"'",i.lower()):
-                continue
-            if re.search(r'[0-9]',i):
-                continue
-            print(i)
-            if not i.lower() in tempdic:
-                # if re.match(r'^\w+$',i.lower()) and not os.path.exists(f'./pronunciation/{i.lower()}'):#下载发音
-                #     os.makedirs(f'./pronunciation/{i.lower()}')
-                #     urllib.request.urlretrieve("http://dict.youdao.com/dictvoice?type=1&audio="+i.lower(),f".\pronunciation\{i.lower()}\{i.lower()}.mp3")
-                inpu=input('"0N/1Y/2Pass"\n')
-                if inpu=='2':
-                    continue
-                if inpu!='0':
-                    tempdic[i.lower()]=1
-                    continue
-            else:
-                meanings=[]
-                for j in sdic[i.lower()]:
-                    if not re.search(r'[一-龟]',j):
-                        wordType=j
-                    else :
-                        meanings.append(wordType+j)
-                meanings.append('')
-            trans=getTranslation(i)
-            print(trans[1])
-            with open('eng.htm','a',encoding="utf-8") as f: #保存数据
-                if i.lower() in tempdic and tempdic[i.lower()]==0:
-                    for j in range(len(meanings)):
-                        print(j,meanings[j])
-                    an=input('which?')
-                    if an=='':
-                        an='0'
-                    f.write(f'Q: {sentence[n][0].replace(i,f"<a style={oneCharacter}display:none{oneCharacter}>.</a><b><u>{i}</u></b><a style={oneCharacter}display:none{oneCharacter}>.</a>")}\nA: {meanings[int(an)]} \n<hr>\n\n')
-                else:
-                    f.write(f'Q: {sentence[n][0].replace(i,f"<a style={oneCharacter}display:none{oneCharacter}>.</a><b><u>{i}</u></b><a style={oneCharacter}display:none{oneCharacter}>.</a>")}\nA: {trans[0]} \n<hr>\n\n')
-            if i.lower()==key:
-                tempdic[i.lower()]=1
-            with open('dic.txt','r') as f:
-                temptempdic=json.loads(f.read())
-            for j in tempdic:
-                if not j in temptempdic:
-                    temptempdic[j]=tempdic[j]
-            temptempdic[i.lower()]=1
-            with open('dic.txt','w') as f:
-                f.write(json.dumps(temptempdic))
+                with open('dic.txt','r') as f:
+                    temptempdic=json.loads(f.read())
+                for j in tempdic:
+                    if not j in temptempdic:
+                        temptempdic[j]=tempdic[j]
+                with open('dic.txt','w') as f:
+                    f.write(json.dumps(temptempdic))
+        # print(f'{totalc-count} left',count)
+        # for i in sentence[n][0].replace('.','').replace('?','').split(): #处理句子
+        #     if i.lower() in tempdic and tempdic[i.lower()]==1 or i in tempdic and tempdic[i]==1 or re.search(r"'",i.lower()):
+        #         continue
+        #     if re.search(r'[0-9]',i):
+        #         continue
+        #     print(i)
+        #     if not i.lower() in tempdic:
+        #         # if re.match(r'^\w+$',i.lower()) and not os.path.exists(f'./pronunciation/{i.lower()}'):#下载发音
+        #         #     os.makedirs(f'./pronunciation/{i.lower()}')
+        #         #     urllib.request.urlretrieve("http://dict.youdao.com/dictvoice?type=1&audio="+i.lower(),f".\pronunciation\{i.lower()}\{i.lower()}.mp3")
+        #         inpu=input('"0N/1Y/2Pass"\n')
+        #         if inpu=='2':
+        #             continue
+        #         if inpu!='0':
+        #             tempdic[i.lower()]=1
+        #             continue
+        #     else:
+        #         meanings=[]
+        #         for j in sdic[i.lower()]:
+        #             if not re.search(r'[一-龟]',j):
+        #                 wordType=j
+        #             else :
+        #                 meanings.append(wordType+j)
+        #         meanings.append('')
+        #     trans=getTranslation(i)
+        #     print(trans[1])
+        #     with open('eng.htm','a',encoding="utf-8") as f: #保存数据
+        #         if i.lower() in tempdic and tempdic[i.lower()]==0:
+        #             for j in range(len(meanings)):
+        #                 print(j,meanings[j])
+        #             an=input('which?')
+        #             if an=='':
+        #                 an='0'
+        #             f.write(f'Q: {sentence[n][0].replace(i,f"<a style={oneCharacter}display:none{oneCharacter}>.</a><b><u>{i}</u></b><a style={oneCharacter}display:none{oneCharacter}>.</a>")}\nA: {meanings[int(an)]} \n<hr>\n\n')
+        #         else:
+        #             f.write(f'Q: {sentence[n][0].replace(i,f"<a style={oneCharacter}display:none{oneCharacter}>.</a><b><u>{i}</u></b><a style={oneCharacter}display:none{oneCharacter}>.</a>")}\nA: {trans[0]} \n<hr>\n\n')
+        #     if i.lower()==key:
+        #         tempdic[i.lower()]=1
+        #     with open('dic.txt','r') as f:
+        #         temptempdic=json.loads(f.read())
+        #     for j in tempdic:
+        #         if not j in temptempdic:
+        #             temptempdic[j]=tempdic[j]
+        #     temptempdic[i.lower()]=1
+        #     with open('dic.txt','w') as f:
+        #         f.write(json.dumps(temptempdic))
 dic=tempdic.copy()
 with open('dic.txt','w') as f:
     f.write(json.dumps(dic))
